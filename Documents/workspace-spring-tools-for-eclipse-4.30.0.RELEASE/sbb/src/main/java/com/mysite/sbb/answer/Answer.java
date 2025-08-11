154a2a98023727ffd4aff51460f02483685858c9
JEONG SUNWOO <sunwoojeong0430@gmail.com>
2025-08-11 19:18:47 +0900

    검색 기능 추가

--- Documents/workspace-spring-tools-for-eclipse-4.30.0.RELEASE/sbb/src/main/java/com/mysite/sbb/answer/Answer.java
+++ Documents/workspace-spring-tools-for-eclipse-4.30.0.RELEASE/sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.answer;
 
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.user.SiteUser;
@@ -12,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -35,4 +37,7 @@ public class Answer {
 	private SiteUser author;
 	
 	private LocalDateTime modifyDate;
+	
+    @ManyToMany
+    Set<SiteUser> voter;
 }
--- Documents/workspace-spring-tools-for-eclipse-4.30.0.RELEASE/sbb/src/main/java/com/mysite/sbb/question/Question.java
+++ Documents/workspace-spring-tools-for-eclipse-4.30.0.RELEASE/sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
@@ -14,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -40,4 +42,7 @@ public class Question {
 	private SiteUser author;
 	
 	private LocalDateTime modifyDate;
+	
+	@ManyToMany
+	Set<SiteUser> voter;
 }
